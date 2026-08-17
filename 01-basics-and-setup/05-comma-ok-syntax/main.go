c936b22f825d83a45ead56a0f5f2ceb53ef4b23f
Siva-K67 <avishawkings@gmail.com>
2026-08-17 22:43:33 +0530

    comma-ok syntax

---
+++ 01-basics-and-setup/05-comma-ok-syntax/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//map for comma ok syntax
+	scores := map[string]int{
+		"go":     90,
+		"python": 85,
+		"c":      99,
+	}
+
+	//ok becomes true since go exists in map
+	value, ok := scores["go"]
+	fmt.Println(value, ok)
+
+	//ok2 is false because c++ isnt in the map
+	//value2 defaults to 0
+	value2, ok2 := scores["C++"]
+	fmt.Println(value2, ok2)
+
+	//now type assertion
+	// is this value the type i think it is ?
+
+	//i is an interface, it can hold any type.
+	//i.(string) means is i a string ?
+	var i interface{} = "hello"
+	s, ok3 := i.(string)
+	fmt.Println(s, ok3)
+
+	// is i an int ??
+	// i sint an int so its false and n defaults to 0
+	n, ok4 := i.(int)
+	fmt.Println(n, ok4)
+
+}
