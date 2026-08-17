35c26123202f8046b72b3353aa95d09b898ad9d1
Siva-K67 <avishawkings@gmail.com>
2026-08-17 21:57:40 +0530

    Types of vars

---
+++ 01-basics-and-setup/03-lexer-and-types/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b float64 = 3.14
+	var c string = "hello"
+	var d bool = true
+
+	fmt.Println(a, b, c, d)
+	fmt.Printf("Type of a : %T\n", a)
+	fmt.Printf("Type of b : %T\n", b)
+	fmt.Printf("Type of c : %T\n", c)
+	fmt.Printf("Type of c : %T\n", d)
+
+}
