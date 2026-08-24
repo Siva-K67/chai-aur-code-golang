df60971341001c810016a93a52f382afcf2f0db2
Siva-K67 <avishawkings@gmail.com>
2026-08-24 15:48:31 +0530

    Server at 8080

---
+++ 05-files-and-web-basics/02-handling-web-requests/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// this exact function signature is called a handler. Every function that
+// responds to a web request in Go has this shape: a ResponseWriter
+// (to send data back) and a *Request (info about what came in).
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from the home page of Ippo Makunochi!")
+	// fmt.Fprintln(w, ...) instead of printing to your terminal, it writes to w —
+	//  which sends it back to the browser as the response.
+}
+
+func main() {
+
+	//this is routing: "when someone visits /, run homeHandler."
+	http.HandleFunc("/", homeHandler)
+	fmt.Println("server starting on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("server error: ", err)
+	}
+}
